f873453aaae7d786b9043594af163473c1985e21
Victor Toulouse <toulousevictor@gmail.com>
2021-10-19 17:13:50 +0200

    Remove unused fragment. Change colors.

--- app/src/main/java/com/vctls/bookmarkkeywords/MainActivity.kt
+++ app/src/main/java/com/vctls/bookmarkkeywords/MainActivity.kt
@@ -38,7 +38,8 @@ class MainActivity : AppCompatActivity() {
         // menu should be considered as top level destinations.
         appBarConfiguration = AppBarConfiguration(
             setOf(
-                R.id.nav_home, R.id.nav_list, R.id.nav_slideshow
+                R.id.nav_home,
+                R.id.nav_list
             ),
             drawerLayout
         )
