89513ab9dee6d7ad175e0162121dde2277fcfac6
Victor Toulouse <toulousevictor@gmail.com>
2021-11-08 19:23:07 +0100

    Remove settings menu. closes #7

--- app/src/main/java/vctls/bookmarkkeywords/MainActivity.kt
+++ app/src/main/java/vctls/bookmarkkeywords/MainActivity.kt
@@ -1,7 +1,6 @@
 package vctls.bookmarkkeywords
 
 import android.os.Bundle
-import android.view.Menu
 import androidx.appcompat.app.AppCompatActivity
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
@@ -55,12 +54,6 @@ class MainActivity : AppCompatActivity() {
 
     fun hasFab(): Boolean {
         return ::fab.isInitialized
-    }
-
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        menuInflater.inflate(R.menu.main, menu)
-        return true
     }
 
     override fun onSupportNavigateUp(): Boolean {
